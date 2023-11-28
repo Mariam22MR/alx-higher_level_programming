a421ae838c8569a011157213bfddca0953a78c4b
Mariam22MR <Xxxsaakk@gmail.com>
2023-11-28 12:37:15 -0800

    a

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,12 +6,9 @@ modluse = number % 10 if number > 10 else number % -10
 
 print("Last digit of {:d} is {:d} and is "
         .format(number, modluse), end="")
-
 if modluse > 5:
     print("greater than 5")
-
 elif modluse == 0:
     print("0")
-
 else:
     print("less than 6 and not 0")
