0c35d322de60e810a280e4b07ec3bdfee9e5122f
Mariam22MR <Xxxsaakk@gmail.com>
2023-11-28 16:54:21 -0800

    a

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for n in range(25, -1, -1):
+    c = n + ord('A')
+    if n % 2 == 1:
+        c += 32
+    print('{:c}'.format(c), end='')
+
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new = ""
+    for x, c in enumerate(str):
+        if x != n:
+            new += c
+        return new
