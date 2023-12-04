8b6948a9678be3be63b865336e8e11f0ed3a718b
Mariam22MR <Xxxsaakk@gmail.com>
2023-12-04 11:53:58 -0800

    a

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     ret = ""
     for n in range(len(my_string)):
-        if (my_string[n] != 'c' and my_string[n] != 'c'):
+        if (my_string[n] == 'c' and my_string[n] == 'c'):
             ret += my_string[n]
     return ret
