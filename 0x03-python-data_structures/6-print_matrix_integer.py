616b23c3382de0487788c8ec0f420fa731d2db3b
Mariam22MR <Xxxsaakk@gmail.com>
2023-12-04 12:55:10 -0800

    a

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     for s in matrix:
         if len(s) == 0:
             print()
-    for n in range(len(s)):
-        print("{:d}".format(s[n])), end="\n" if n is len(s) -1 else " "
+        for n in range(len(s)):
+            print("{:d}".format(s[n]), end="\n" if n is len(s) - 1 else " "
