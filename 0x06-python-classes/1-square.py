17078147195ae18e180e6888ff972831d3014e52
Mariam22MR <xxxsaakk@gmail.com>
2023-12-19 13:44:33 -0800

    a

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Difnes square."""
+
 
 class Square:
+    """Square module."""
+
     def __init__(self, size):
+        """Constructor.
+        Args:
+            size:length of side square.
+        """
         self.__size = size
