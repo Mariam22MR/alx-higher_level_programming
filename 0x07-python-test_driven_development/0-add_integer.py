f2ff0d429e93852d37ec0db82ff2e1d771b4849f
Mariam22MR <xxxsaakk@gmail.com>
2024-01-02 01:47:13 -0800

    a

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that define function for int add.
+"""
+
+
+def add_integer(a, b=98):
+    """adds two integers.
+
+    Args:
+        a: first integer.
+        b: second integer, default 98.
+
+    Raises:
+        TypeError: if a, b are not int, float.
+
+    Returns:
+        sum of two integers.
+    """
+
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
