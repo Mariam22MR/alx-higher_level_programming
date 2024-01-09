7ebbf2ae35e65a6c1f7e4c4763a9c065f6d3c941
Mariam22MR <xxxsaakk@gmail.com>
2024-01-09 14:28:22 -0800

    a

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,7 +6,7 @@ class Student:
     """representation of student."""
 
     def __init__(self, first_name, last_name, age):
-        """initialization of new Student. """
+        """initialization of new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
