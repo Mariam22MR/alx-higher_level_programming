781b83b7b859d6ad8b2276e7bbf10071cbd65b78
Mariam22MR <xxxsaakk@gmail.com>
2024-01-09 14:52:27 -0800

    a

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """adds all args to python list then save to file."""
+
 from sys import argv
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
