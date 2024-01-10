5cdfbfd8f7f1ac3897564f407306efd735f2af84
Mariam22MR <xxxsaakk@gmail.com>
2024-01-10 11:26:40 -0800

    a

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Defines a matrix division function."""
+"""
+Defines a matrix division function.
+"""
 
 
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix.
 
     Args:
-        matrix (list): A list of lists of ints or floats.
-        div (int/float): The divisor.
+        matrix (list): list of lists of ints or floats.
+        div (int/float): divisor.
     Raises:
-        TypeError: If the matrix contains non-numbers.
-        TypeError: If the matrix contains rows of different sizes.
-        TypeError: If div is not an int or float.
-        ZeroDivisionError: If div is 0.
+        TypeError: if matrix contains non-numbers.
+        TypeError: if matrix contains rows of different sizes.
+        TypeError: if div not int or float.
+        ZeroDivisionError: if div 0.
     Returns:
-        A new matrix representing the result of the division.
+        new matrix representing result of division.
     """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,19 +8,14 @@ def say_my_name(first_name, last_name=""):
     """method for print first name  and last name.
 
     Args:
-        first_name: string.
-        last_name: string.
-
+        first_name (str): first name to print.
+        last_name (str): last name to print.
     Raises:
-        TypeError: if first_name & last_name not strings.
+        TypeError: if either of first_name or last_name not strings.
     """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
 
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
-    print('My name is {:s} {:s}'.format(first_name, last_name))
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
+    print('My name is {} {}'.format(first_name, last_name))
