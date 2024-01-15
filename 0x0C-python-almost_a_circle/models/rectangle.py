ef137d2b2cadd651d2080cd2cefde053123dc9b8
Mariam22MR <xxxsaakk@gmail.com>
2024-01-15 14:31:03 -0800

    a

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,11 +16,11 @@ class Rectangle(Base):
             x (int): x coordinate of new Rectangle.
             y (int): y coordinate of new Rectangl.
         """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
