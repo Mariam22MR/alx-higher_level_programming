ea2af7ba18d280c4c47257cdc4da50a7aed6b98b
Mariam22MR <xxxsaakk@gmail.com>
2024-02-14 13:00:25 -0800

    a

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,7 @@
 -- Creates table with constraint on column to have unique value
 -- for each row, which is never empty when value isn't given
 CREATE TABLE IF NOT EXISTS unique_id(
-	id INT DEFAULT 1 UNIQUE,
+	id INT DEFAULT 1,
 	name VARCHAR(256),
+	UNIQUE(id)
 );
