d80d8de2404c493a3f45eaf22c682d0d7fa044cb
Mariam22MR <xxxsaakk@gmail.com>
2024-03-15 02:45:25 -0700

    a

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ def main():
     """ function now starts when imported """
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
                          passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states ORDER BY states.id""")
+    start = db.cursor()
+    start.execute("""SELECT * FROM states ORDER BY states.id""")
     rows = start.fetchall()
     for row in rows:
         print(row)
